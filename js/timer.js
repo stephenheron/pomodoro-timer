790e27385f3cfe778817ba1d6ecb690302fd4702
Stephen Heron <stephen.heron@gmail.com>
2012-11-28 16:35:22 +0000

    Used JSHint on timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,22 +1,23 @@
 function Timer(endTime) {
+  "use strict";
   var currentTime = new Date();
   this.startTime = currentTime.getTime();
-  this.endTime = endTime
-  this.startingDiff = this.endTime - this.startTime
+  this.endTime = endTime;
+  this.startingDiff = this.endTime - this.startTime;
 
   this.timeRemaining = function(){
     var currentTime = new Date();
-    return (this.endTime - currentTime.getTime())
-  }
+    return (this.endTime - currentTime.getTime());
+  };
 
   this.percentageToEndTime = function(){
-    var percentage =  100 - ((this.timeRemaining() / this.startingDiff) * 100)
+    var percentage =  100 - ((this.timeRemaining() / this.startingDiff) * 100);
     if (percentage > 100){
       return 100;
     } else {
-      return percentage
+      return percentage;
     }
-  }
+  };
 
   this.isReached = function(){
     if(this.timeRemaining() < 0){
@@ -24,33 +25,33 @@ function Timer(endTime) {
     } else {
       return false;
     }
-  }
+  };
 
   this.hoursMinutesSecondsRemaining = function(){
     var totalSec = this.timeRemaining() / 1000;
-    var hours = parseInt( totalSec / 3600 ) % 24;
-    var minutes = parseInt( totalSec / 60 ) % 60;
-    var seconds = Math.round(totalSec % 60);
+    var hours = parseInt(totalSec / 3600, 10) % 24;
+    var minutes = parseInt(totalSec / 60, 10) % 60;
+    var seconds = Math.round(totalSec % 60, 10);
 
     if(seconds < 0){
       seconds = 0;
     }
 
     var hoursMinutesSeconds = {
-      "hours": hours < 10 ? "0" + hours : hours
-      ,"minutes": minutes < 10 ? "0" + minutes : minutes
-      ,"seconds":seconds  < 10 ? "0" + seconds : seconds
-    }
+      "hours": hours < 10 ? "0" + hours : hours,
+      "minutes": minutes < 10 ? "0" + minutes : minutes,
+      "seconds":seconds  < 10 ? "0" + seconds : seconds
+    };
     return hoursMinutesSeconds;
-  }
+  };
 
   this.tick = function(){
     var hoursMinutesSeconds = this.hoursMinutesSecondsRemaining();
-    console.log(hoursMinutesSeconds["minutes"] + ":" + hoursMinutesSeconds["seconds"]);
+    console.log(hoursMinutesSeconds.minutes + ":" + hoursMinutesSeconds.seconds);
     if(this.isReached()){
       return false;
     } else {
       return true;
     }  
-  }
+  };
 }
