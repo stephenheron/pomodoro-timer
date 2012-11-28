f015c172aa0a784ea2cb632748a89c992700d6e9
Stephen Heron <stephen.heron@gmail.com>
2012-11-28 16:41:31 +0000

    JSHinted timer_manager.js

--- js/timer_manager.js
+++ js/timer_manager.js
@@ -1,22 +1,23 @@
 function TimerManager() {
-  this.pomodoroCount = 0
+  "use strict";
+  this.pomodoroCount = 0;
   this.maxPomodoroCount = 4;
   this.currentTimerType = null;
 
   this.startPomodoroTimer = function(){
-    console.log("Starting Pomodoro")
+    console.log("Starting Pomodoro");
     this.startTimer(this.pomodoroEndTime(), "pomodoro");
-  }
+  };
 
   this.startShortBreakTimer = function(){
-    console.log("Starting Short Break")
+    console.log("Starting Short Break");
     this.startTimer(this.shortBreakEndTime(), "short");
-  }
+  };
 
   this.startLongBreakTimer = function(){
-    console.log("Starting Long Break")
+    console.log("Starting Long Break");
     this.startTimer(this.longBreakEndTime(), "long");
-  }
+  };
 
   this.startTimer = function(endTime, type){
     this.setCurrentTimerType(type);
@@ -45,46 +46,46 @@ function TimerManager() {
         }
       }
     }
-  }
+  };
 
 
   this.setCurrentTimerType = function(type){
     if(type === "pomodoro" || type === "short" || type === "long"){
       this.currentTimerType = type;
     }
-  }
+  };
 
   this.pomodoroEndTime = function(){
     return this.currentTime() + this.getPomodoroTime();
-  }
+  };
 
   this.shortBreakEndTime = function(){
     return this.currentTime() + this.getShortBreakTime();
-  }
+  };
 
   this.longBreakEndTime = function(){
     return this.currentTime() + this.getLongBreakTime();
-  }
+  };
 
   this.getPomodoroTime = function(){
     //return (25 * 60) * 1000;
     return (0.1 * 60) * 1000;
-  }
+  };
 
   this.getShortBreakTime = function(){
     //return (5 * 60) * 1000;
     return (0.1 * 60) * 1000;
-  }
+  };
 
   this.getLongBreakTime = function(){
     //return (15 * 60) * 1000;
     return (0.1 * 60) * 1000;
-  }
+  };
 
   this.currentTime = function(){
     var currentTime = new Date();
     return currentTime.getTime();
-  }
+  };
 }
 
 var tm = new TimerManager();
